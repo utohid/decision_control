dac02e78767ce30f1eac45e2451e7a4bc3f99159
utohid <noortohid78@gmail.com>
2022-02-03 21:25:07 +0530

    Grid java

---
+++ Array_fold/grid.java
@@ -0,0 +1,14 @@
+public class grid {
+    public static void main(String args[])
+    {
+        int i;int j;
+        for (i=0;i<=10;i++)
+        {
+            for (j=0;j<=10;j++)
+            {
+                System.out.print(" - ");
+            }
+            System.out.println();
+        }
+    }
+}
